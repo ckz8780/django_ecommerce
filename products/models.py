51a8c462c92bc35fe45dbb1074aeedf8309014f1
Chris Zielinski <ckz8780@gmail.com>
2019-10-20 17:25:20 -0400

    Added category model, fixtures, and products page styling

--- products/models.py
+++ products/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+class Category(models.Model):
+	name = models.CharField(max_length=254)
+	friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+	def __str__(self):
+		return f'{self.name} ({self.friendly_name})'
+
 class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
