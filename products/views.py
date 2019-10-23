8c127fd3e1e974aa5a77452dbda3cb9c6e356414
Chris Zielinski <ckz8780@gmail.com>
2019-10-23 15:23:29 -0400

    added basic product filtering

--- products/views.py
+++ products/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from .models import Product
 
 def all_products(request):
-    products = Product.objects.all()
-    return render(request, 'products/products.html', {'products': products})
+	products = Product.objects.all()
+	if request.GET:
+		if 'sort' in request.GET:
+			sortkey = request.GET['sort']
+			if 'direction' in request.GET:
+				direction = request.GET['direction']
+				if direction == 'desc':
+					sortkey = f'-{sortkey}'
+			
+			products = products.order_by(sortkey)
+
+		if 'category' in request.GET:
+			categories = request.GET['category'].split(',')
+			products = products.filter(category__name__in=categories)
+
+	return render(request, 'products/products.html', {'products': products})
