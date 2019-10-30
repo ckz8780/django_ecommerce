57b753cd34564b455dd0c914a58ceaf3b1dda1e7
Chris Zielinski <ckz8780@gmail.com>
2019-10-30 13:42:17 -0400

    added product management

--- products/models.py
+++ products/models.py
@@ -7,15 +7,18 @@ class Category(models.Model):
 	def __str__(self):
 		return f'{self.name} ({self.friendly_name})'
 
+	def get_friendly_name(self):
+		return self.friendly_name
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254, default='')
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to='images')
-    
-    def __str__(self):
-        return self.name
+	category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+	sku = models.CharField(max_length=254, null=True, blank=True)
+	name = models.CharField(max_length=254, default='')
+	description = models.TextField()
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+	image_url = models.URLField(max_length=1024, null=True, blank=True)
+	image = models.ImageField(upload_to='images', null=True, blank=True)
+	
+	def __str__(self):
+		return self.name
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 
 from .models import Product
+from .forms import ProductForm
 
 def all_products(request):
 	products = Product.objects.all()
@@ -30,5 +31,64 @@ def all_products(request):
 			queries = Q(name__icontains=query) | Q(description__icontains=query)
 			products = products.filter(queries)
 
-
 	return render(request, 'products/products.html', {'products': products, 'search_term': query})
+
+def product_management(request):
+	
+	if request.method == 'POST':
+		form = ProductForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Successfully added product')
+			return redirect(reverse('product_management'))
+
+		messages.error(request, 'Failed to add product. Make sure your form is valid')
+		return redirect(reverse('product_management'))
+
+	
+	form = ProductForm()
+
+	template = 'products/product_management.html'
+	context = {
+		'form': form
+	}
+
+	return render(request, template, context)
+
+def edit_product(request, product_id):
+	if request.user.is_superuser:
+		try:
+			product = Product.objects.get(id=product_id)
+			form = ProductForm(instance=product)
+			messages.info(request, f'You are editing {product.name}')
+
+		except Product.DoesNotExist:
+			messages.error(request, 'Product not found. Are you sure it exists?')
+			form = ProductForm()
+	else:
+		messages.error(request, 'You must be a store manager to do this.')
+
+	template = 'products/product_management.html'
+	context = {
+		'form': form
+	}
+
+	return render(request, template, context)
+
+
+def delete_product(request, product_id):
+	if request.user.is_superuser:
+		try:
+			Product.objects.get(id=product_id).delete()
+			messages.success(request, 'Product deleted!')
+			return redirect(reverse('products'))
+
+		except Product.DoesNotExist:
+			messages.error(request, 'Product not found. Please try deleting it through the admin panel.')
+			return redirect(reverse('products'))
+
+	else:
+		messages.error(request, 'You must be a store manager to do this.')
+		return redirect(reverse('products'))
+
+
