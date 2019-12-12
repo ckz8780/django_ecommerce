745c4730d707fdbd358eb8de68e7c4a2ec8b37e2
Christopher Zielinski <ckz8780@gmail.com>
2019-12-12 10:03:25 -0500

    Added profiles app and profile FK to Order model

---
+++ profiles/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining
+    personal information and order history
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a user profile for all new users
+    """
+
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
