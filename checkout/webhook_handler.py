fda74bd7ca20bf28ab6636c1ed85f988aa08a805
Christopher Zielinski <ckz8780@gmail.com>
2020-02-22 09:27:08 -0500

    updated generic webhook response

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,7 +18,7 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
 
