f9ca10ee7159800aae1e38d19e267b5025e79ab0
Christopher Zielinski <ckz8780@gmail.com>
2020-02-22 09:13:10 -0500

    Added webhook url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import checkout, cache_checkout_data, checkout_success
+from .webhooks import webhook
 
 urlpatterns = [
     path('', checkout, name='checkout'),
     path('cache_checkout_data/', cache_checkout_data, name='cache_checkout_data'),
     path('checkout_successs/<order_number>/', checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -13,7 +13,7 @@ class StripeWH_Handler:
     
     def __init__(self, request):
         self.request = request
-        
+
 
     def handle_event(self, event):
         """
@@ -26,11 +26,11 @@ class StripeWH_Handler:
         """
         Handle the "payment_intent.succeeded" webhook from Stripe
         """
-        return HttpResponse(status=200)
+        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
 
 
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the "payment_intent.payment_failed" webhook from Stripe
         """
-        return HttpResponse(status=200)
+        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
